4b6a8b872dd2ac6836287b5595cd699438718ebb
Khang Vo <vodkhang@gmail.com>
2018-05-07 15:40:09 +1000

    First meteor simple-todo project

---
+++ .demeteorized/bundle/programs/server/app/app.js
@@ -0,0 +1,28 @@
+var require = meteorInstall({"server":{"main.js":function(require,exports,module){
+
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+// server/main.js                                                    //
+//                                                                   //
+///////////////////////////////////////////////////////////////////////
+                                                                     //
+let Meteor;
+module.watch(require("meteor/meteor"), {
+  Meteor(v) {
+    Meteor = v;
+  }
+
+}, 0);
+Meteor.startup(() => {// code to run on server at startup
+});
+///////////////////////////////////////////////////////////////////////
+
+}}},{
+  "extensions": [
+    ".js",
+    ".json"
+  ]
+});
+require("/server/main.js");
+//# sourceURL=meteor://💻app/app/app.js
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1ldGVvcjovL/CfkrthcHAvc2VydmVyL21haW4uanMiXSwibmFtZXMiOlsiTWV0ZW9yIiwibW9kdWxlIiwid2F0Y2giLCJyZXF1aXJlIiwidiIsInN0YXJ0dXAiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEsSUFBSUEsTUFBSjtBQUFXQyxPQUFPQyxLQUFQLENBQWFDLFFBQVEsZUFBUixDQUFiLEVBQXNDO0FBQUNILFNBQU9JLENBQVAsRUFBUztBQUFDSixhQUFPSSxDQUFQO0FBQVM7O0FBQXBCLENBQXRDLEVBQTRELENBQTVEO0FBRVhKLE9BQU9LLE9BQVAsQ0FBZSxNQUFNLENBQ25CO0FBQ0QsQ0FGRCxFIiwiZmlsZSI6Ii9hcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBNZXRlb3IgfSBmcm9tICdtZXRlb3IvbWV0ZW9yJztcblxuTWV0ZW9yLnN0YXJ0dXAoKCkgPT4ge1xuICAvLyBjb2RlIHRvIHJ1biBvbiBzZXJ2ZXIgYXQgc3RhcnR1cFxufSk7XG4iXX0=
